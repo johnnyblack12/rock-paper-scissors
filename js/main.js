417ed63fd19673dfb62d21b56333a0f21ff7ddf0
johnnyblack12 <jfebbo5@gmail.com>
2021-03-22 20:52:16 -0400

    First draft

--- js/main.js
+++ js/main.js
@@ -8,56 +8,56 @@ function computerPlay() {
     } else {
         computerHand = 'Scissors';
     }
-}
+};
 
-computerPlay();
 
 let playerHand;
 let win = false;
-let lose = false;
+let loss = false;
 function playRound() {
+    computerPlay();
     while(playerHand !== 'rock' && playerHand !== 'paper' && playerHand !== 'scissors') {
         playerHand = prompt("Rock, Paper, or Scissors?").toLowerCase();
     }
-    if (playerHand == 'rock' && computerHand == 'Scissors' || playerHand == 'paper' && computerHand == 'Rock' || playerHand == 'scissors' && computerHand == 'Paper') {
+    if (playerHand == 'rock' && computerHand == 'Scissors' || playerHand == 'paper' && computerHand == 'Rock' ||
+            playerHand == 'scissors' && computerHand == 'Paper') {
         win = true;
-    } else if (playerHand == 'rock' && computerHand == 'Paper' || playerHand == 'paper' && computerHand == 'Scissors' || playerHand == 'scissors' && computerHand == 'Rock') {
-        lose = true;
+    } else if (playerHand == 'rock' && computerHand == 'Paper' || playerHand == 'paper' && computerHand == 'Scissors' ||
+            playerHand == 'scissors' && computerHand == 'Rock') {
+        loss = true;
     }
-}
+};
 
 let upperPH;
 
-
 let playerScore = 0;
 let computerScore = 0;
-while(playerScore < 5 && computerScore < 5) {
-    playRound();
-    upperPH = playerHand[0].toUpperCase() + playerHand.substring(1);
-    if (win == true) {
-        playerScore++;
-        console.log(upperPH + ' beats ' + computerHand + '. You win!');
-        console.log('Player: ' + playerScore);
-        console.log('Computer: ' + computerScore);
-        console.log(' ');
-    } else if (lose == true) {
-        computerScore++;
-        console.log(computerHand + ' beats ' + upperPH + '. You lose.');
-        console.log('Player: ' + playerScore);
-        console.log('Computer: ' + computerScore);
-        console.log(' ');
-    } else {
-        console.log('Opponent chose ' + computerHand + '. Play again: ');
-        console.log(' ');
-    }
-    if (playerScore == 5) {
-        console.log('You\'ve won 5 rounds. You win the game!')
-    } else if (computerScore == 5) {
-        console.log('The computer won 5 rounds. You are a loser!')
-    }
-    playerHand = null;
-    computerHand = null;
-    win = false;
-    lose = false;
-    computerPlay();
-}
+// while(playerScore < 5 && computerScore < 5) {
+//     playRound();
+//     upperPH = playerHand[0].toUpperCase() + playerHand.substring(1);
+//     if (win == true) {
+//         playerScore++;
+//         console.log(upperPH + ' beats ' + computerHand + '. You win!');
+//         console.log('Player: ' + playerScore);
+//         console.log('Computer: ' + computerScore);
+//         console.log(' ');
+//     } else if (loss == true) {
+//         computerScore++;
+//         console.log(computerHand + ' beats ' + upperPH + '. You lose.');
+//         console.log('Player: ' + playerScore);
+//         console.log('Computer: ' + computerScore);
+//         console.log(' ');
+//     } else {
+//         console.log('Opponent chose ' + computerHand + '. Play again: ');
+//         console.log(' ');
+//     }
+//     if (playerScore == 5) {
+//         console.log('You have won 5 rounds. You win the game!')
+//     } else if (computerScore == 5) {
+//         console.log('The computer has won 5 rounds. You are a loser!')
+//     }
+//     playerHand = null;
+//     computerHand = null;
+//     win = false;
+//     loss = false;
+// };
