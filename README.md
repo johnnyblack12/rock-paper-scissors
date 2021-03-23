dc09be1a1f0557bfaccda59b23656b4793284673
johnnyblack12 <jfebbo5@gmail.com>
2021-03-23 15:10:05 -0400

    Version 1 final

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Simple game to practice and present dynamic web page building with HTML/CSS/JS
+Simple game to practice and present CSS styling and DOM manipulation with JavaScript.
 
 Neon rock, paper, scissors images attributed to: nightwolfdezines, https://www.vecteezy.com/vector-art/691497-rock-paper-scissors-neon-icons
--- js/main.js
+++ js/main.js
@@ -1,63 +1,149 @@
-let computerHand;
-function computerPlay() {
-    let decision = Math.random();
-    if (decision <= 0.3333) {
-        computerHand = 'Rock';
-    } else if (decision > 0.3333 && decision <= 0.6666) {
-        computerHand = 'Paper';
-    } else {
-        computerHand = 'Scissors';
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+let stationL = document.querySelector('.stationL');
+let stationC = document.querySelector('.stationC');
+let stationR = document.querySelector('.stationR');
+let userScore = document.querySelector('#user-score');
+let computerScore = document.querySelector('#computer-score');
+let messageArea = document.querySelector('.battleC');
+let score;
+
+let decision;
+let clicked = false;
+
+function scoreUp(playerScore) {
+    score = Number(playerScore.textContent);
+    score++;
+    playerScore.textContent = score;
+}
+
+function winCheck() {
+    if (Number(userScore.textContent) < 5 && Number(computerScore.textContent) < 5) {
+        clicked = false;
+    } else if (Number(userScore.textContent) == 5){
+        messageArea.textContent = 'YOU WIN!'
+    } else if (Number(computerScore.textContent) == 5){
+        messageArea.textContent = 'YOU LOSE!'
     }
-};
+}
 
-
-let playerHand;
-let win = false;
-let loss = false;
-function playRound() {
-    computerPlay();
-    while(playerHand !== 'rock' && playerHand !== 'paper' && playerHand !== 'scissors') {
-        playerHand = prompt("Rock, Paper, or Scissors?").toLowerCase();
+rock.addEventListener('click', () => {
+    if (clicked == false) {
+        clicked = true;
+        rock.classList.add('rock-image-clicked');
+        stationL.classList.replace('stationL', 'battleL');
+        decision = Math.random();
+        if (decision < 0.5) {
+            paper.classList.add('paper-image-clicked');
+            stationC.classList.replace('stationC', 'battleR');
+            paper.setAttribute('id', 'computer-win');
+            rock.setAttribute('id', 'user-lose');
+            setTimeout(() => {
+                scoreUp(computerScore);
+                stationC.classList.replace('battleR', 'stationC');
+                stationL.classList.replace('battleL', 'stationL');
+                rock.removeAttribute('id', 'user-lose');
+                rock.classList.remove('rock-image-clicked');
+                paper.removeAttribute('id', 'computer-win');
+                paper.classList.remove('paper-image-clicked');
+                winCheck();
+            }, 3000);
+        } else {
+            scissors.classList.add('scissors-image-clicked');
+            stationR.classList.replace('stationR', 'battleR');
+            rock.setAttribute('id', 'user-win');
+            scissors.setAttribute('id', 'computer-lose');
+            setTimeout(() => {
+                scoreUp(userScore);
+                stationR.classList.replace('battleR', 'stationR');
+                stationL.classList.replace('battleL', 'stationL');
+                rock.removeAttribute('id', 'user-win');
+                rock.classList.remove('rock-image-clicked');
+                scissors.removeAttribute('id', 'computer-lose');
+                scissors.classList.remove('scissors-image-clicked');
+                winCheck();
+            }, 3000);            
+        }
     }
-    if (playerHand == 'rock' && computerHand == 'Scissors' || playerHand == 'paper' && computerHand == 'Rock' ||
-            playerHand == 'scissors' && computerHand == 'Paper') {
-        win = true;
-    } else if (playerHand == 'rock' && computerHand == 'Paper' || playerHand == 'paper' && computerHand == 'Scissors' ||
-            playerHand == 'scissors' && computerHand == 'Rock') {
-        loss = true;
+});
+
+paper.addEventListener('click', () => {
+    if (clicked == false) {
+        clicked = true;
+        paper.classList.add('paper-image-clicked');
+        stationC.classList.replace('stationC', 'battleL');
+        decision = Math.random();
+        if (decision < 0.5) {
+            scissors.classList.add('scissors-image-clicked');
+            stationR.classList.replace('stationR', 'battleR');
+            scissors.setAttribute('id', 'computer-win');
+            paper.setAttribute('id', 'user-lose');
+            setTimeout(() => {
+                scoreUp(computerScore);
+                stationR.classList.replace('battleR', 'stationR');
+                stationC.classList.replace('battleL', 'stationC');
+                paper.removeAttribute('id', 'user-lose');
+                paper.classList.remove('paper-image-clicked');
+                scissors.removeAttribute('id', 'computer-win');
+                scissors.classList.remove('scissors-image-clicked');
+                winCheck();
+            }, 3000);
+        } else {
+            rock.classList.add('rock-image-clicked');
+            stationL.classList.replace('stationL', 'battleR');
+            paper.setAttribute('id', 'user-win');
+            rock.setAttribute('id', 'computer-lose');
+            setTimeout(() => {
+                scoreUp(userScore);
+                stationL.classList.replace('battleR', 'stationL');
+                stationC.classList.replace('battleL', 'stationC');
+                paper.removeAttribute('id', 'user-win');
+                paper.classList.remove('paper-image-clicked');
+                rock.removeAttribute('id', 'computer-lose');
+                rock.classList.remove('rock-image-clicked');
+                winCheck();
+            }, 3000);            
+        }
     }
-};
+});
 
-let upperPH;
-
-let playerScore = 0;
-let computerScore = 0;
-// while(playerScore < 5 && computerScore < 5) {
-//     playRound();
-//     upperPH = playerHand[0].toUpperCase() + playerHand.substring(1);
-//     if (win == true) {
-//         playerScore++;
-//         console.log(upperPH + ' beats ' + computerHand + '. You win!');
-//         console.log('Player: ' + playerScore);
-//         console.log('Computer: ' + computerScore);
-//         console.log(' ');
-//     } else if (loss == true) {
-//         computerScore++;
-//         console.log(computerHand + ' beats ' + upperPH + '. You lose.');
-//         console.log('Player: ' + playerScore);
-//         console.log('Computer: ' + computerScore);
-//         console.log(' ');
-//     } else {
-//         console.log('Opponent chose ' + computerHand + '. Play again: ');
-//         console.log(' ');
-//     }
-//     if (playerScore == 5) {
-//         console.log('You have won 5 rounds. You win the game!')
-//     } else if (computerScore == 5) {
-//         console.log('The computer has won 5 rounds. You are a loser!')
-//     }
-//     playerHand = null;
-//     computerHand = null;
-//     win = false;
-//     loss = false;
-// };
+scissors.addEventListener('click', () => {
+    if (clicked == false) {
+        clicked = true;
+        scissors.classList.add('scissors-image-clicked');
+        stationR.classList.replace('stationR', 'battleL');
+        decision = Math.random();
+        if (decision < 0.5) {
+            rock.classList.add('rock-image-clicked');
+            stationL.classList.replace('stationL', 'battleR');
+            rock.setAttribute('id', 'computer-win');
+            scissors.setAttribute('id', 'user-lose');
+            setTimeout(() => {
+                scoreUp(computerScore);
+                stationL.classList.replace('battleR', 'stationL');
+                stationR.classList.replace('battleL', 'stationR');
+                scissors.removeAttribute('id', 'user-lose');
+                scissors.classList.remove('scissors-image-clicked');
+                rock.removeAttribute('id', 'computer-win');
+                rock.classList.remove('rock-image-clicked');
+                winCheck();
+            }, 3000);
+        } else {
+            paper.classList.add('paper-image-clicked');
+            stationC.classList.replace('stationC', 'battleR');
+            scissors.setAttribute('id', 'user-win');
+            paper.setAttribute('id', 'computer-lose');
+            setTimeout(() => {
+                scoreUp(userScore);
+                stationC.classList.replace('battleR', 'stationC');
+                stationR.classList.replace('battleL', 'stationR');
+                scissors.removeAttribute('id', 'user-win');
+                scissors.classList.remove('scissors-image-clicked');
+                paper.removeAttribute('id', 'computer-lose');
+                paper.classList.remove('paper-image-clicked');
+                winCheck();
+            }, 3000);            
+        }
+    }
+});
